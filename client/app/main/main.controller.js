e478f478642d877731448b53556d3b33dba7c5d1
Jean Claudette Ambait <jeanclaudetteambait@gmail.com>
2014-09-03 15:13:13 +0800

    Create layout

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -4,8 +4,11 @@ angular.module('siteAngularApp')
   .controller('MainCtrl', function ($scope, $http) {
     $scope.awesomeThings = [];
 
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-    });
+    // $http.get('/api/things').success(function(awesomeThings) {
+    //   $scope.awesomeThings = awesomeThings;
+    // });
 
+    // $http.get('/api/twitterline').success(function(twitterfeed) {
+    //   $scope.twitterfeed = twitterfeed;
+    // });
   });
